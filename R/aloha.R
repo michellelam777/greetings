639b3818f022fcda925ab757ad7799cf8726311e
reedalexandria <reed.alexandria@outlook.com>
2022-08-23 22:38:57 +0000

    update color

--- R/aloha.R
+++ R/aloha.R
@@ -1,5 +1,5 @@
 
-#' Says Aloah to a friend
+#' Says Aloha to a friend
 #'
 #' @param name a character
 #' @param print a logical
@@ -8,13 +8,22 @@
 #' @export
 #'
 #' @examples say_aloha("Allison")
-say_aloha <- function(name) {
+say_aloha <- function(name, color) {
 
   message <- paste("Aloha,",
                    name,
                    emo::ji("palm_tree"),
                    emo::ji("sunny"),
                    emo::ji("ocean"))
-
-  cat(crayon::bgBlue(message))
+if(color == "green") {
+  cat(crayon::bgGreen(message))
 }
+else if(color == "cyan") {
+  cat(crayon::bgCyan(message))
+}
+else {
+    print("Please input 'green' or 'cyan' for the color")
+ }
+}
+
+
